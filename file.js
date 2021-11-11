3e511601427c332515edfcb47fcba043bb1d1c5c
sebolsky <73398026+sebolsky@users.noreply.github.com>
2021-11-11 17:57:03 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,11 +1,20 @@
-document.getElementById("count").addEventListener("click", function() {
-   
+function wyliczDawkeLeku(){
 
+  let waga = document.getElementById("weight").value;
+  
+  let typLeku = document.getElementById("names_of_medications").value;
 
+  console.log(waga);
+  console.log(typLeku);
 
+if(typLeku == 'paracetamol'){
+parseInt(weight) * 0,01 + "g na kg masy ciała";
 
+}else if(typLeku == 'ibuprofen'){
+  parseInt(weight) * 0,05 + "g na kg masy ciała"
+}else{
+  
+}
 
-
-
-    
-  });
+ 
+}
