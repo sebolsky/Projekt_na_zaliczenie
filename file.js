8912bc8da6c774132777b349f525ccc59032c7dd
sebolsky <73398026+sebolsky@users.noreply.github.com>
2021-11-14 11:43:45 +0100

    Update file.js

--- file.js
+++ file.js
@@ -8,18 +8,18 @@ function wyliczDawkeLeku(){
   console.log(typLeku);
 
 if(isNaN(waga)){
-  document.getElementById("amount").innerHTML = "Niedozwolona wartość!"
+  document.getElementById("amount").innerHTML = "Niedozwolona wartość!";
 }
 else if(waga <= 20 || waga >= 150){
-  document.getElementById("amount").innerHTML = "Podano nieprawidłową wagę!"
+  document.getElementById("amount").innerHTML = "Podano nieprawidłową wagę!";
 }
 
 else if(typLeku == 'paracetamol'){
-let dawkaLeku = waga * 0.01
+let dawkaLeku = waga * 0.01;
 document.getElementById("amount").innerHTML= `Rekomendowana dawka leku wynosi ${dawkaLeku} g paracetamolu`;
 
 }else if(typLeku == 'ibuprofen'){
-  let dawkaLeku = waga * 0.005
+  let dawkaLeku = waga * 0.005;
   document.getElementById("amount").innerHTML= `Rekomendowana dawka leku wynosi ${dawkaLeku} g ibuprofenu`;
 }
   
